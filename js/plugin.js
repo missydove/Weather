204c4c7dcb873f155c7653e0b07947a02c671f7a
missydove <missydovefitness@gmail.com>
2017-03-26 21:05:48 -0700

    png

--- js/plugin.js
+++ js/plugin.js
@@ -1,42 +1,44 @@
 // Paste jQuery Plugin Code Here
 // Get and store Geo Location lat/long coordinates
-navigator.geolocation.getCurrentPosition( 
-  
-  function(position) {
-  
-  // wait a few seconds to receive location
-  var lat = position.coords.latitude;
-  var long = position.coords.longitude;
-  
-  console.log( lat, long); 
+if ("geolocation") in navigator) {
+    navigator.geolocation.getCurrentPosition(Function(position) {
+        loadWeather(position.coords.latitude + ',' + position.coords.longitude);
+    });
 
-      
-      function loadweather (location, woeid) {
-          $.simpleWeather({
-              location: location,
-              woeid: woeid,
-              unit: 'c'
-              success: function(weather) {
-              city = weather.city;
-              temp = weather.temp+'&deg;'
-              wcode +' img class="weathericon" src="img/weathericon/' + weather.code+'.svg">';
-          
-          wind = '<p>' + weather.wind.speed + '</p><p>' + weathe.units.speed + '</p';
-          }
-              
-            humidity = weather.humidity + ' %' ;})
-      
-      $(".location").text(city);
-          $(".temperature").html(temp);
-          $(".climate_bg").html(wcode);
-          $(".windspeed").html(wind);
-          $("humidity").text(humidity);
-          
-      },
-          
-          
-          
-      
-      
-  
+} else {
+    loadWeather("Spokane, WA,"
+        ");
+        $(document).ready(function () {
+            setInterval(getWeather, 10000)
+        });
+
+    function loadweather(location, woeid) {
+        $.simpleWeather({
+                    location: location,
+                    woeid: woeid,
+                    unit: 'c'
+                    success: function (weather) {
+                        city = weather.city;
+                        temp = weather.temp + '&deg;';
+                        wcode + ' <img class="weathericon" src="img/weathericons/' + weather.code + '.svg">';
+
+                        wind = '<p>' + weather.wind.speed + '</p><p>' + weather.units.speed + '</p';
+
+
+                        humidity = weather.humidity + ' %';
+                    })
+
+                $(".location").text(city); $(".temperature").html(temp); $(".climate_bg").html(wcode); $(".windspeed").html(wind); $("humidity").text(humidity);
+
+            },
+
+            error: function (error) {
+                $(".error").html('<p>') + error + '</p>');
+    }
+
+
+
+
 });
+
+}
--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,3 @@
-$.simpleWeather({
-    location: 99216,
-    unit: 'f',
-    success: function(weather) {
-      // Entire weather object
-      console.log(weather);
-      
-      // Display Data
-      $('.temp').text(weather.temp);
-      $('.city').text(weather.city);
-      $('img').attr('src', weather.image);
-      
-      // Condition Code
-      // Reference Condition Codes
-      // https://developer.yahoo.com/weather/documentation.html#codes
-      console.log(weather.code);
-      
-    
-// Get and store Geo Location lat/long coordinates
-navigator.geolocation.getCurrentPosition( 
-  
-  function(position) {
-  
-  // wait a few seconds to receive location
-  var lat = position.coords.latitude;
-  var long = position.coords.longitude;
-  
-  console.log( lat, long); 
-
-  
-});
 
 
 
